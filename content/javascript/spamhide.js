375f1a027c629b2327613802f26f2c50ef36b768
Barry Steyn <barry.steyn@gmail.com>
2015-02-07 13:01:08 -0800

    Generate Pelican site

--- content/javascript/spamhide.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $('#mail').replaceWith(function() {
-        var s = $(this).text().replace(' [at] ', '@').replace(' [dot] ', '.').replace(' [dot] ','.');
-        return '<a href="mailto:' + s + '">' + s + '</a>';
-    });
-});
